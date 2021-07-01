222135836af08171bc02747a2499519477db6bff
DevTipsy <thibaults65@yahoo.com>
2021-07-01 22:53:02 +0200

    delete files Please enter the commit message for your changes. Lines starting

--- imageconvert.php
+++
@@ -1,8 +0,0 @@
-<?php
-Header ('Content-type: image/png');
-$resource = imagecreatefrompng ('logo_gbaf.png');
-imagealphablending($resource, false);
-imagesavealpha($resource, true);
-imagecolortransparent($resource);
-imagepng($resource);
-?>
