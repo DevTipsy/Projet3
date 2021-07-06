008f781c96208ad4c45b7e0159901f81b3b8f3d6
DevTipsy <thibaults65@yahoo.com>
2021-07-06 02:25:49 +0200

    final update & motdepasseoublie

--- motdepasseoublie.php
+++ motdepasseoublie.php
@@ -1,75 +1,68 @@
 <?php
-	include("connexion.php");
+include("connexion.php");
+session_start();
+@$username=$_POST["username"];
+@$reponse=$_POST["reponse"];
+@$password=$_POST["password"];
+@$valider=$_POST["valider"];
+$message="";
 
-	try{
-		$pdoConnect=new PDO("mysql:host=localhost;dbname=projet3","root","root");
+if(isset($valider)){
+	if(empty($username)) $message.="<li>Username invalide!</li>";
+	if(empty($reponse)) $message.="<li>Réponse secrète invalide!</li>";
+	if(empty($password)) $message.="<li>Mot de passe invalide!</li>";
+	if(empty($message)){
+			
+		$sql = "SELECT id_user, username, reponse FROM account WHERE username = :username AND reponse = :reponse";
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':reponse', $reponse, PDO::PARAM_STR);
+		$execute = $stmt->execute();
+        $count = $stmt->rowCount();
+
+		if($count == 1) {
+			$query = "UPDATE account SET password = :password WHERE username = :username";
+			$stmt = $pdo->prepare($query);
+			$stmt->bindParam(':username', $username, PDO::PARAM_STR);
+			$stmt->bindParam(':password', md5($password), PDO::PARAM_STR);
+			$execute = $stmt->execute();
+			session_destroy();
+			session_start();
+			$_SESSION["success"] = "Mot de passe mis à jour avec succès";
+			header("location:login.php");
+			exit();
+		} else {
+			$message = "Nom d'utilisateur ou réponse à la question secrète invalide";
+		}	
 	}
-	catch(PDOException $e){
-		echo $e->getMessage();
-	}
-
-	session_start();
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-
-	$username=$_POST["username"];
-	$reponse=$_POST["reponse"];
-
-	try {
-            $sql = "SELECT id_user, username, reponse FROM account WHERE username = :username AND reponse = :reponse";
-            $stmt = $pdoConnect->prepare($sql);
-            $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-            $stmt->bindParam(':reponse', $reponse, PDO::PARAM_STR);
-            $execute = $stmt->execute();
-            $count = $stmt->rowCount();
-if($count == 1) {
-                $query = "UPDATE account SET password = :password WHERE username = :username";
-                $stmt = $pdoConnect->prepare($query);
-                $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-                $stmt->bindParam(':password', $password, PDO::PARAM_STR);
-                $execute = $stmt->execute();
-                session_destroy();
-                session_start();
-                $_SESSION["success"] = "Mot de passe mis à jour avec succès";
-
-  		header("location: login.php");
-                exit();
-            } else {
-                $login_err = "Nom d'utilisateur ou réponse à la question secrète invalide";
-            }
-        } catch (PDOException $e) {
-            echo "Error : ".$e->getMessage();
-        }
-
 }
 ?>
-<!DOCYTPE html>
+<!DOCTYPE html>
 <html>
-	    <header>
-            <div id="header">
-                <meta charset="utf-8" />
-                <link rel="stylesheet" type="text/css" href="css/index.css" />
-                <link rel="stylesheet" media="(max-width: 575.98px)" href="css/media.css" />
-                <link rel="stylesheet" media="smartphones(max-width: 767.98px)" href="css/media.css" />
-                <link rel="stylesheet" media="tablets(max-width: 991.98px)" href="css/media.css" />
-                <link rel="stylesheet" media="desktops(max-width: 1199.98px)" href="css/media.css" />
-
-                            <a class="logoh" href="page_daccueil.php"><img alt="logo" src="images/logo_gbaf.png" width="100px" height="100px"></a><span style="font-weight: bold ; font-size: 26px;">  Réinitialisation du mot de passe</span>
-
+	<header>
+        <div id="header">
+			<meta charset="utf-8" />
+			<link rel="stylesheet" type="text/css" href="css/index.css" />
+			<link rel="stylesheet" media="(max-width: 575.98px)" href="css/media.css" />
+			<link rel="stylesheet" media="smartphones(max-width: 767.98px)" href="css/media.css" />
+			<link rel="stylesheet" media="tablets(max-width: 991.98px)" href="css/media.css" />
+			<link rel="stylesheet" media="desktops(max-width: 1199.98px)" href="css/media.css" />
+			<a class="logoh" href="page_daccueil.php"><img alt="logo" src="images/logo_gbaf.png" width="100px" height="100px"></a><span style="font-weight: bold ; font-size: 26px;">  Réinitialisation du mot de passe</span>
+		</div>
     </header>
 
 	<body onLoad="document.fo.username.focus()">
-		<form class="formu" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-			<div class="label">Username (email)</div>
+		<form class="formu" method="post" action="">
+			<div class="label">Nom d'utilisateur</div>
 			<input  class="forml" type="text" name="username" value="" />
-			<div class="label">Réponse secrète</div>
+			<div class="label">Réponse à la question secrète</div>
 			<input  class="forml" type="text" name="reponse" />
-			<div class="label"> Mot de passe</div>
-			<input  class="forml" type="text" name="motdepasse" value="" />
-
+			<div class="label">Nouveau mot de passe</div>
+			<input  class="forml" type="password" name="password" />
 			<input class="formn" type="submit" name="valider" value="Envoyer le nouveau mot de passe" /><br>
 		</form>
-			<?php if(!empty($message)){ ?>
-				<div class="message"><?php echo $message ?></div>
-			<?php } ?>
+		<?php if(!empty($message)){ ?>
+			<div class="message"><?php echo $message ?></div>
+		<?php } ?>
 	</body>
 </html>
--- update.php
+++ update.php
@@ -1,19 +1,9 @@
 <?php
-    session_start();
-    if(@$_SESSION["autoriser"]!="oui"){
-        header("location:login.php");
-        exit();
-    }
-try {
-        $pdoConnect = new PDO("mysql:host=localhost;dbname=projet3","root","root");
-    } catch (PDOException $exc) {
-        echo $exc->getMessage();
-        exit();
-    }
-
+include("connexion.php");
+session_start();
+ 
 if(isset($_POST['update']))
 {
-
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
     $username = $_POST['username'];
@@ -21,55 +11,50 @@ if(isset($_POST['update']))
     $question = $_POST['question'];
     $reponse = $_POST['reponse'];
 
-
-    $query = "UPDATE account SET 'nom'=:nom, 'prenom'=:prenom, 'username'=:username, 'password'=:password, 'question'=:question, 'reponse'=:reponse WHERE 'id_user'=:id_user";    
-    $pdoResult = $pdoConnect->prepare($query);
-$pdoResult->bindParam(':id_user', $_SESSION["id"], PDO::PARAM_INT);
-$pdoResult->bindParam(':nom', $nom, PDO::PARAM_STR);
-$pdoResult->bindParam(':prenom', $prenom, PDO::PARAM_STR);
-$pdoResult->bindParam(':username', $username, PDO::PARAM_STR);
-$pdoResult->bindParam(':password', $password, PDO::PARAM_STR);
-$pdoResult->bindParam(':question', $question, PDO::PARAM_STR);
-$pdoResult->bindParam(':reponse', $reponse, PDO::PARAM_STR);
-$pdoExec = $pdoResult->execute();
-var_dump($pdoExec);
+    $query = "UPDATE account SET nom=:nom, prenom=:prenom, username=:username, password=:password, question=:question, reponse=:reponse WHERE id_user=:id_user";    
+    $pdoResult = $pdo->prepare($query);
+    $pdoResult->bindParam(':id_user', $_SESSION["id"], PDO::PARAM_INT);
+    $pdoResult->bindParam(':nom', $nom, PDO::PARAM_STR);
+    $pdoResult->bindParam(':prenom', $prenom, PDO::PARAM_STR);
+    $pdoResult->bindParam(':username', $username, PDO::PARAM_STR);
+    $pdoResult->bindParam(':password', md5($password), PDO::PARAM_STR);
+    $pdoResult->bindParam(':question', $question, PDO::PARAM_STR);
+    $pdoResult->bindParam(':reponse', $reponse, PDO::PARAM_STR);
+    $pdoExec = $pdoResult->execute();
 
     if($pdoExec)
     {
-        echo 'Mise à jour effectuée';
-    }else{
-        echo 'Echec de la mise à jour';
+        $message = "Mise à jour effectuée";
+    } else {
+        $message = "Echec de la mise à jour";
     }
 }
-$query = "SELECT * FROM account WHERE id_user = :id_user";
-$stmt = $pdoConnect->prepare($query);
-$stmt->bindParam(':id_user', $_SESSION["id"], PDO::PARAM_INT);
-$execute = $stmt->execute();
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $query = "SELECT * FROM account WHERE id_user = :id_user";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':id_user', $_SESSION["id"], PDO::PARAM_INT);
+    $execute = $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html>
     <header>
-            <div id="header">
-                <meta charset="utf-8" />
-                <link rel="stylesheet" type="text/css" href="css/index.css" />
-                <link rel="stylesheet" media="(max-width: 575.98px)" href="css/media.css" />
-                <link rel="stylesheet" media="smartphones(max-width: 767.98px)" href="css/media.css" />
-                <link rel="stylesheet" media="tablets(max-width: 991.98px)" href="css/media.css" />
-                <link rel="stylesheet" media="desktops(max-width: 1199.98px)" href="css/media.css" />
-
-                            <a class="logoh" href="page_daccueil.php"><img alt="logo" src="images/logo_gbaf.png" width="100px" height="100px"></a>
-
-                            <a class="bonjour" href="update.php"><img alt="profil" src="images/profil.png" width="30px" height="30px"></a> <?=$_SESSION["nomPrenom"]?><br>
-                            <a style="color: black;" href="deconnexion.php">Me déconnecter</a>
-
+        <div id="header">
+            <meta charset="utf-8" />
+            <link rel="stylesheet" type="text/css" href="css/index.css" />
+            <link rel="stylesheet" media="(max-width: 575.98px)" href="css/media.css" />
+            <link rel="stylesheet" media="smartphones(max-width: 767.98px)" href="css/media.css" />
+            <link rel="stylesheet" media="tablets(max-width: 991.98px)" href="css/media.css" />
+            <link rel="stylesheet" media="desktops(max-width: 1199.98px)" href="css/media.css" />
+            <a class="logoh" href="page_daccueil.php"><img alt="logo" src="images/logo_gbaf.png" width="100px" height="100px"></a>
+            <a class="bonjour" href="update.php"><img alt="profil" src="images/profil.png" width="30px" height="30px"></a> <?=$_SESSION["nomPrenom"]?><br>
+            <a style="color: black;" href="deconnexion.php">Me déconnecter</a>
+        </div>
     </header>
 
     <body>
-
         <div align="center">
-            <form class="formu" action="update.php" method="post">
+            <form class="formu" action="" method="post">
                 <label>Nom</label>
                 <input class="forml" type="text" value="<?php echo $row['nom']; ?>" name="nom">
                 <label>Prénom</label>
@@ -77,13 +62,16 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
                 <label>Username (email)</label>
                 <input class="forml" type="text" value="<?php echo $row['username']; ?>" name="username">
                 <label>Nouveau mot de passe</label>
-                <input class="forml" type="text" value="<?php echo $row['password']; ?>" name="password">
+                <input class="forml" type="password" value="" name="password">
                 <label>Question secrète</label>
                 <input class="forml" type="text" value="<?php echo $row['question']; ?>" name="question">
                 <label>Réponse secrète</label>
                 <input class="forml" type="text" value="<?php echo $row['reponse']; ?>" name="reponse"><br>                
                 <input class="formn" type="submit" name="update" value="Mettre à jour mon profil" />
             </form>
+            <?php if(!empty($message)){ ?>
+			    <div class="message"><?php echo $message ?></div>
+		    <?php } ?>
         </div>
     </body>
 </html>
